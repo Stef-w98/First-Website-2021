0b4a8b7595a2698b1d883c409bba08c6b22621d2
Stef <stef.wouters22@gmail.com>
2021-06-12 19:56:04 +0200

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-
+//gets input from forms and presses submit button
 var submit = document.getElementById("isubmit");
 console.log(submit);
 submit.addEventListener("click", getInputValue );
 
 function getInputValue(){
 
-  var inputVal = document.getElementById("uname").value;
-
-
-  alert(inputVal);
+  var u = document.getElementById("uname").value;
+  var f = document.getElementById("fname").value;
+  var l = document.getElementById("lname").value;
+  alert(u + ", " + f + ", " + l);
 }
